e10a6033325ac8383461db1cb6c0a7eb649a4b59
Sneha728 <snehaavala05@gmail.com>
2025-01-20 17:49:46 +0530

    Update axios.js

--- frontend/ChattyFront/src/lib/axios.js
+++ frontend/ChattyFront/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:9000/api",
+    baseURL: "https://chattyapp-backend.onrender.com/",
     withCredentials:true,
 });
 
